10ceb3d78a3498011e6200ad03d49fcbd62255ba
JamieParker3009 <43817091+JamieParker3009@users.noreply.github.com>
2020-01-13 15:41:14 +0000

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            for (Int32 i = 0; i < 100; i++)
+            {
+                num++;
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# FizzBuzz
+
+My implementation of this game using C#.
+
+In addition to the C# code, there is a matching JSP diagram.
